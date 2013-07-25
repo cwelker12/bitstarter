57a10960d33cbb8dc91bbd6a35322d2fcefaaa1e
Chris Welker <cjwelker@gmail.com>
2013-07-25 00:57:13 +0000

    Updated checks.json and grader.js

--- grader.js
+++ grader.js
@@ -52,20 +52,48 @@ out[checks[ii]] = present;
 return out;
 };
 
+var checkUrl = function(htmlfile, checksfile) {
+var instr = htmlfile.toString();
+$ = cheerio.load(instr);
+var checks = loadChecks(checksfile).sort();
+var out = {};
+for(var ii in checks) {
+var present = $(checks[ii]).length > 0);
+out[checks[ii]] = present;
+}
+var outJson = JSON.stringify(out, null, 4);
+console.log(outJson);
+return out;
+};
+
 var clone = function(fn) {
 //workaround for commander.js issue.
 //http://stackoverflow.com/a/6772648
-return fn.bind({});
+//return fn.bind({});
+return fn.lFile;
 };
 
 if(require.main == module) {
 program
 .option('-c, --checks <check_file>', 'Path to checks.json', clone(asertFileExists), CHCKESFILE_DEFAULT)
 .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+.option('-u, --url <url_file>', 'Command line defined URL',clone(doNothingFn), URL_DEFAULT)
 .parse(process.argv);
+
+if(program.url) {
+rest.get(program.url).on('complete', function(result) {
+if (result instanceof Error) {
+console.log("Page not Found");
+process.exit(1);
+} else {
+console.log("Checking" + program.url);
+checkUrl(result, program.checks);
+}
+});
+} else {
 var checkJson = checkHtmlFile(program.file, program.checks);
 var outJson = JSON.stringify(checkJson, null, 4);
-console.log(outJson);
+console.log(outJson); }
 } else {
 exports.checkHtmlFile=checkHtmlfile;
 }
